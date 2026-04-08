bb505e9880cf3c03b46b5d0901185d8596b88ea7
Victor <Victor@DESKTOP-KHPNUNT>
2026-04-08 17:56:23 -0300

    Correção

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,4 +1,9 @@
-﻿namespace MauiAppTempoAgora
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+
+
+
+namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
@@ -9,16 +14,40 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            try 
+            {
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
+                    if (t != null)
+                    {
+                        string dados_previsao = "";
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                        dados_previsao = $"Latitude: {t.lat} \n " +
+                                         $"Longitude: {t.lon} \n " +
+                                         $"Nascer do Sol: {t.sunrise} \n " +
+                                         $"Por do Sol: {t.sunset} \n " +
+                                         $"Temp Máx: {t.temp_max} \n " +
+                                         $"Temp Min: {t.temp_min} \n ";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                        lbl_res.Text = dados_previsao;
+                    }
+                    else
+                    {
+                        lbl_res.Text = "Sem dados de Previsão";
+                    }
+                }
+                else
+                {
+                    lbl_res.Text = "Preencha a Cidade";
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "OK");
+            }
         }
     }
 
---
+++ MauiAppTempoAgora/Services/DataService.cs
@@ -0,0 +1,47 @@
+﻿using MauiAppTempoAgora.Models;
+using Newtonsoft.Json.Linq;
+
+namespace MauiAppTempoAgora.Services
+{
+    public class DataService
+    {
+        public static async Task<Tempo?> GetPrevisao(string cidade)
+        {
+            Tempo t = null;
+
+            string chave = "6135072afe7f6cec1537d5cb08a5a1a2";
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={chave}&units=metric&lang=pt_br";
+            using (HttpClient Client = new HttpClient())
+            {
+                HttpResponseMessage resp = await Client.GetAsync(url);
+                if (resp.IsSuccessStatusCode)
+                {
+                    string json = await resp.Content.ReadAsStringAsync();
+                    var rascunho = JObject.Parse(json);
+
+                    DateTime time = new();
+                    DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+                    DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunset"]).ToLocalTime();
+                    t = new ()
+                    {
+                        lon = (double)rascunho["coord"]["lon"],
+                        lat = (double)rascunho["coord"]["lat"],
+                        visibility = (int)rascunho["visibility"],
+                        temp_min = (double)rascunho["main"]["temp_min"],
+                        temp_max = (double)rascunho["main"]["temp_max"],
+                        sunrise = sunrise.ToString(),
+                        sunset =  sunset.ToString(),
+                        description = (string)rascunho["weather"][0]["description"],
+                        id = (int)rascunho["weather"][0]["id"],
+                        speed = (double)rascunho["wind"]["speed"],
+                        main = (string)rascunho["weather"][0]["main"],
+                    };
+                }
+                return t;
+
+
+            }
+
+        }
+    }
+}
